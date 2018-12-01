d43c78dede3cd65795d1dd48a43dbb54d200b16e
giulianonetto <giuliano_nf@hotmail.com>
2018-12-01 19:01:25 -0200

    add humans script and generated figs

--- rmd-files/Scripts/S4_mPFR_DePianto2015.R
+++ rmd-files/Scripts/S4_mPFR_DePianto2015.R
@@ -1,20 +1,24 @@
-  # Load packages and set working dir
+######  Load packages and set working dir ######
 {
   library(limma)
   library(convert)
   library(vsn)
+  library(car)
   library(arrayQualityMetrics)
   library(ggpubr)
   library(ggfortify)
+  library(ggrepel)
   library(stringr)
   library(dplyr)
-  library(affy)
+  library(GEOquery)
+  library(reshape2)
   setwd("/home/giulianonetto/windows/tcc/rmd-files")
   
 }
 
 
-###### Load data and preprocessing ######
+
+###### Preprocessing - YOU CAN SKIP THIS ######
 
 
   # Takes a while!
@@ -100,12 +104,18 @@
   
 }
 
-  # YOU CAN START FROM HERE - LOAD PROCESSED DATA
+
+###### LOAD PREPROCESSED DATA ######
+
 {
   eset <- readRDS("data/GSE53845/suppdata/ExpressionSet.rds")
   pheno <- pData(eset)
 }
 
+
+
+
+###### QUALITY CONTROL - TAKES FOREVER!!! ######
   # ArrayQualityMetrics - takes forever!!
 {
   # Array Quality Metrics outlier detection
@@ -114,19 +124,32 @@
                                  reporttitle = "DePianto2015 - all", force = TRUE)
 }
   
-  # Compute PCAs - with outliers
+
+
+###### EXPLORATORY DATA ANALYSIS ######
+outliers.vec <- colnames(eset) %in% c("GSM1302034 / IPF transplant", 
+                                      "GSM1302054 / control necropsy",
+                                      "GSM1302065 / IPF biopsy")
+
+# Compute PCAs - with outliers
 {
-  outliers.vec <- colnames(eset) %in% c("GSM1302034 / IPF transplant", 
-                                        "GSM1302054 / control necropsy",
-                                        "GSM1302065 / IPF biopsy")
-  
+
   PCA.all <- eset@assayData$exprs %>% t() %>% prcomp(scale. = T, center = F)
-  loadings.df <- data.frame(Load = PCA.all$sdev, Eigen = 1:length(PCA.all$sdev))
+  PCA.all.sum <- summary(PCA.all)$importance
+  loadings.df <- data.frame(VAR = PCA.all.sum[2,1:10] * 100, 
+                            PCs = 1:10, 
+                            stringsAsFactors = F)
   pp1 <- autoplot(PCA.all, x = 1, y = 2, 
                   data = pheno, colour = "Source", 
                   shape = "Treatment", size = 3)
   pp2 <- ggplot(loadings.df,
-                aes(y = Load, x = Eigen)) + geom_point()
+                aes(y = VAR, 
+                    x = PCs)) + 
+    geom_point() + geom_line(color = "blue", group = 1) +
+    xlab("Principal Components") +
+    ylab("Variance Explained (%)") +
+    scale_x_continuous(breaks = 1:10,
+                     labels = paste0("PC", 1:10))
   plots <- ggarrange(pp1, pp2, ncol = 2)
   annotate_figure(plots, 
                   top = text_grob("PCA - IPF patients - with outliers\n",
@@ -137,15 +160,31 @@
   # Compute PCAs - without outliers
 {
   PCA.clean <- eset[,!outliers.vec]@assayData$exprs %>% t() %>% prcomp(scale. = T, center = F)
-  loadings.df <- data.frame(Load = PCA.clean$sdev, Eigen = 1:length(PCA.clean$sdev))
+  PCA.clean.sum <- summary(PCA.clean)$importance
+  loadings.df <- data.frame(VAR = PCA.clean.sum[2,1:10] * 100, 
+                            PCs = 1:10, 
+                            stringsAsFactors = F)
   pp1 <- autoplot(PCA.clean, x = 1, y = 2, 
-           data = pheno[!outliers.vec,], colour = "Source", 
-           shape = "Treatment", size = 3)
-  pp2 <- ggplot(loadings.df,
-                  aes(y = Load, x = Eigen)) + geom_point()
-  plots <- ggarrange(pp1, pp2, ncol = 2)
+                  data = pheno[!outliers.vec,],
+                  colour = "Treatment", size = 3)
+  pp2 <- autoplot(PCA.clean, x = 3, y = 2, 
+                  data = pheno[!outliers.vec,],
+                  colour = "Treatment", size = 3) 
+  pp3 <- autoplot(PCA.clean, x = 1, y = 3, 
+                  data = pheno[!outliers.vec,],  
+                  colour = "Treatment", size = 3)
+  
+  pp4 <- ggplot(loadings.df,
+                  aes(y = VAR, x = PCs)) + 
+    geom_point() + geom_line(color = "blue", group = 1) +
+    xlab("Principal Components") +
+    ylab("Variance Explained (%)") +
+    scale_x_continuous(breaks = 1:10,
+                       labels = paste0("PC", 1:10))
+  plots <- ggarrange(pp1, pp2, pp3, pp4, ncol = 2, nrow = 2, 
+                     common.legend = T)
   annotate_figure(plots, 
-                  top = text_grob("PCA - IPF patients - without outliers\n",
+                  top = text_grob("PCA - IPF and Control patients\n",
                                   face = "bold", size = 20, 
                                   vjust = 1))
 }
@@ -160,7 +199,7 @@
 
 ####### mPFR Calculations #######
 
-# Load data
+# Load Clean Data
 {
   eset <- readRDS("data/GSE53845/suppdata/ExpressionSetClean.rds")
   pheno <- pData(eset)
@@ -173,10 +212,8 @@
   
   # Calculate mPFR
   
-  mPFR <- function(sets_data.frame, exprs_matrix, Time, Treat) {
-    # rownames(exprs_matrix) <- toupper(rownames(exprs_matrix)) # normalize cases
+  mPFR <- function(sets_data.frame, exprs_matrix, Source, Treat) {
     mpfr_data.frame <- data.frame()
-    # rownames(mpfr_data.frame) <- colnames(exprs_matrix)
     for (i in 1:nrow(sets_data.frame)) {
       set1.vec <- sets_data.frame[i,1] %>% unlist()
       set2.vec <- sets_data.frame[i,2] %>% unlist()
@@ -188,7 +225,7 @@
       mpfr_data.frame[1:length(mpfr),i] <- mpfr
     }
     colnames(mpfr_data.frame) <- rownames(sets_data.frame)
-    mpfr_data.frame$times <- Time
+    mpfr_data.frame$source <- Source
     mpfr_data.frame$treatment <- Treat
     return(mpfr_data.frame)
   }
@@ -215,29 +252,27 @@
       print("Outliers check done!")
     }
     else print("Skip outlier tests...")
-    p <- ggplot(df, aes(x = treatment, y = value))
-    stat <- stat_compare_means(comparisons = list(c("bleomycin", "control")),
+    p <- ggplot(df, aes(x = treatment, y = value, color = treatment))
+    stat <- stat_compare_means(comparisons = list(c("IPF", "control")),
                                method = method,
                                aes(label = ..p.signif..))
     fac <- facet_wrap(~ variable)
-    title <- ggtitle(titles)
     if (point == FALSE) {
-      p + stat + fac + geom_boxplot() + title
+      p + stat + fac + geom_boxplot()
     }
     else {
-      p + stat + fac + geom_point() + geom_boxplot(alpha = 0.25) + title
+      p + stat + fac + geom_point() + geom_boxplot(alpha = 0.25)
     }
   }
   
   # Fit linear models to check 
   # correlation btw mPFR candidates and IPF conditions
   
-  fit_mPFR <- function(df, day = 3, Nvars = 28){
+  fit_mPFR <- function(df, Nvars = 28){
     fitdf <- data.frame(Rsquared = vector('numeric'),
                         pval = vector('numeric'),
                         Estimate = vector('numeric'), 
                         stringsAsFactors = F)
-    df = df[df$times == day, ]
     for (i in 1:Nvars) {
       var.name <- colnames(df)[i]
       fit <- lm(df[,i] ~ treatment, data = df)
@@ -251,19 +286,24 @@
     }
     return(fitdf)
   }
-  
 }
 
 # Define clusters from Bauer2015
 
-## note that ~ 70 - 80% of cluster's genes are NOT
-## present in this dataset
+"
+
+        note that ~ 70 - 80% of cluster's genes are NOT
+        present in this dataset
+
+"
+
 {
   # define genesets from masigpro clusters
 
   hcluster <- readRDS("data/GSE48455/suppdata/hcluster_masigpro_bauer2015.rds")
   c1a = names(hcluster$cut)[which(hcluster$cut == 1)] %>% gsub(pattern = "_", replacement = ".") %>% str_to_title()
   c1 <- c1a[c1a %in% rownames(genexp)]; print(length(c1)/length(c1a) * 100)
+  c1not <- c1a[!c1a %in% c1]
   c2a = names(hcluster$cut)[which(hcluster$cut == 2)] %>% gsub(pattern = "_", replacement = ".") %>% str_to_title()
   c2 <- c2a[c2a %in% rownames(genexp)]; print(length(c2)/length(c2a) * 100)
   c3a = names(hcluster$cut)[which(hcluster$cut == 3)] %>% gsub(pattern = "_", replacement = ".") %>% str_to_title()
@@ -290,10 +330,14 @@
   in the S1-3 clusters (Il13ra1.3 is not present,
   but Il13ra1.0-2 are)
 "
+
 {
+  # we take the mean expression for 3 probes that map to IL13ra1
+  # REMOVED: Ccl12 (not in the dataset)
+  genexp["Il13ra1",] <- colMeans(genexp[grepl("Il13ra1.*", rownames(genexp)),])
   
   s1l <- list("S1" = c("Il12b"), 
-              "S2" = c("Ccl12", "Ccr7", "Ccl22", "Ccl24"),
+              "S2" = c("Ccr7", "Ccl22", "Ccl24"),
               "S3" = c("Ccl2", "Ccr2", "Il1b"), 
               "C1" = c1, "C2" = c2, "C3" = c4, "C4" = c5, 
               "C5" = c1, "C6" = c2, "C7" = c4, "C8" = c5, 
@@ -304,8 +348,8 @@
               "C24" = c2, "C25" = c2
   )
   s2l <- list("S1" = c("Arg1"), 
-              "S2" = c("Il13ra1.3", "Il13ra2", "Cd36", "Cxcr1", "St7"),
-              "S3" = c("Apoe", "Mmp14", "Lbp", "Il1rl1.1"), 
+              "S2" = c("Il13ra1", "Il13ra2", "Cd36", "Cxcr1", "St7"),
+              "S3" = c("Apoe", "Mmp14", "Lbp", "Il1rl1"), 
               "C1" = c3, "C2" = c3, "C3" = c3, "C4" = c3,
               "C5" = c6, "C6" = c6, "C7" = c6, "C8" = c6,               
               "C9" = c7, "C10" = c7, "C11" = c7, "C12" = c7,
@@ -324,12 +368,8 @@
   Adapt functions for the current dataset metadata
 "
 {
-  
-  mpfr.all <- mPFR(df, genexp, pheno$Times, pheno$Cy3)
-  mpfr.all.long <- melt(mpfr.all, id.vars = c("times", "treatment"))
-  mpfr.all$times <- factor(mpfr.all$times)
-  mpfr.all$treatment <- factor(mpfr.all$treatment, levels = c("bleomycin", "control"))
-  contrasts(mpfr.all$treatment) <- c("bleomycin" = 1, "control" = 0)
+  mpfr.all <- mPFR(df, genexp, pheno$Source, pheno$Treatment)
+  mpfr.all.long <- melt(mpfr.all, id.vars = c("source", "treatment"))
 }
 
 
@@ -341,54 +381,83 @@
     facet_wrap(~ variable) +
     ggplot2::stat_qq() + ggplot2::stat_qq_line(color = "red")
   # Overall Density
+  plotDensities(t(mpfr.all[,1:28])) # maybe you should normalize again
   ggplot(mpfr.all.long, aes(value)) +
     geom_density() + xlim(-10,10)
+  ggplot(mpfr.all.long, aes(sample = value)) +
+    ggplot2::stat_qq() + ggplot2::stat_qq_line(color = 'red')
+  "
+  NORMALIZE AGAIN???? - normalizeVSN, quantile normalization...
+  library(preprocessCore)
+  mpfr.norm <- normalize.quantiles(t(mpfr.all[,1:28]))
+  qqnorm(mpfr.norm)
+  ggplot(mpfr.all.long, aes(sample = value)) +
+    ggplot2::stat_qq() + ggplot2::stat_qq_line(color = 'red')
+  "
 }
 
 
 # PCA
 
 {
-autoplot(prcomp(mpfr.all[,1:28], scale. = T), 
-         data = mpfr.all, colour = "treatment", 
-         size = 2)
-autoplot(prcomp(mpfr.all[,1:28], scale. = T), 
-         data = mpfr.all, colour = "times", 
-         size = 2, shape = "treatment")
+
+  pca <- prcomp(mpfr.all[,1:28], scale. = T, center = F)
+  PCA.sum <- summary(pca)$importance
+  loadings.df <- data.frame(VAR = PCA.sum[2,1:10] * 100, 
+                            PCs = 1:10, 
+                            stringsAsFactors = F)
+  
+  ppp1 <- autoplot(pca, data = mpfr.all, colour = "treatment", 
+                   size = 3)
+  ppp2 <- autoplot(pca, x = 3, y = 2, data = mpfr.all, 
+                   size = 3, colour = "treatment")
+  ppp3 <- autoplot(pca, x = 1, y = 3, data = mpfr.all, 
+                   size = 3, colour = "treatment")
+  ppp4 <- ggplot(loadings.df, aes(y = VAR, x = PCs)) + 
+                 geom_point() + geom_line(color = "blue", group = 1) +
+                 xlab("Principal Components") +
+                 ylab("Variance Explained (%)") +
+                 scale_x_continuous(breaks = 1:10,
+                                    labels = paste0("PC", 1:10))
+  plots <- ggarrange(ppp1, ppp2, ppp3, ppp4, ncol = 2, nrow = 2, 
+                     common.legend = T)
+  annotate_figure(plots, 
+                  top = text_grob("PCA - IPF and Control patients\n",
+                                  face = "bold", size = 20, 
+                                  vjust = 1))
 }
 
 # Boxplots
-
+"
+    SERIA LEGAL MOSTRAR O N EM CADA FACET!!!
+    TAMBEM MODELAR CORRELACAO ENTRE VARIABLES DE HUMANOS E RATOS!!!!
+"
 {
-  day = 7
-  ivars <- 25; nvars <- 30
-  filtro = mpfr.all.long$variable %in% unique(mpfr.all.long$variable)[ivars:nvars] &
-    mpfr.all.long$times %in% day
+  ivars <- 13; nvars <- 24
+  filtro = mpfr.all.long$variable %in% unique(mpfr.all.long$variable)[ivars:nvars]
   form <- value ~ treatment
-  
   plot_signif(mpfr.all.long, form,
               filtro, outTest = T,  method = "wilcox.test",
-              titles = str_glue("Day {day}"), point = T)
-  
+              point = T)
 }
 
 # Modelos
 
 {
-  day = 7
-  dat <- fit_mPFR(mpfr.all, day = day) 
+  dat <- fit_mPFR(mpfr.all) 
   dat$variables <- rownames(dat)
 }
 
 # volcano
 
 {
-ggplot(dat, aes(x = Estimate, y = -log10(pval), label = variables)) +
+ggplot(dat, aes(x = Estimate, y = -log10(pval), 
+                label = variables, size = Rsquared)) +
   geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) +
   geom_hline(yintercept = -log10(0.05), linetype = "dashed", alpha = 0.5) + 
-  theme(legend.title = element_blank()) +
+  labs(size = "R squared", color = NULL) +
   geom_text_repel(force = T) +
-  xlab("Bleomycin Effect")
+  xlab("IPF effect") + xlim(-6, 6)
 }
 
 # pval vs rsquared
@@ -403,17 +472,17 @@ ggplot(dat, aes(x = Estimate, y = -log10(pval), label = variables)) +
 # rsquared vs bleo effect (Estimate)
 
 {
-ggplot(dat, aes(x = Rsquared, y = Estimate, 
-                label = variables)) +
-  geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
-  geom_text_repel(
-    data = dat[dat$Rsquared >= 0.5, ],
-    segment.alpha =  0.3) + 
-  geom_vline(xintercept = 0.95, alpha = 0.5, linetype = "dashed", color = "blue") +
-  theme(legend.title = element_blank()) +
-  annotate("text", x = 0.91, y = 5.5, color = "blue", alpha = 0.5,
-           label = "R^2 == 0.95", parse = TRUE) +
-  ylab("Bleomycin Effect")
+  ggplot(dat, aes(x = Rsquared, y = Estimate, 
+                  label = variables)) +
+    geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
+    geom_text_repel(
+      data = dat[dat$Rsquared >= 0.5, ],
+      segment.alpha =  0.3) + 
+    geom_vline(xintercept = 0.95, alpha = 0.5, linetype = "dashed", color = "blue") +
+    theme(legend.title = element_blank()) +
+    annotate("text", x = 0.85, y = 5.5, color = "blue", alpha = 0.5,
+             label = "R^2 == 0.95", parse = TRUE) +
+    ylab("IPF Effect")
 }
 
 
@@ -454,11 +523,8 @@ ggplot(dat, aes(x = Rsquared, y = Estimate,
 
 {
   
-  mpfr.all <- mPFR(df, genexp, pheno$Times, pheno$Cy3)
-  mpfr.all.long <- melt(mpfr.all, id.vars = c("times", "treatment"))
-  mpfr.all$times <- factor(mpfr.all$times)
-  mpfr.all$treatment <- factor(mpfr.all$treatment, levels = c("bleomycin", "control"))
-  contrasts(mpfr.all$treatment) <- c("bleomycin" = 1, "control" = 0)
+  mpfr.all <- mPFR(df, genexp, pheno$Source, pheno$Treatment)
+  mpfr.all.long <- melt(mpfr.all, id.vars = c("source", "treatment"))
 }
 
 
@@ -475,67 +541,107 @@ ggplot(mpfr.all.long, aes(value)) +
 
 
 # PCA
+
+"
+  Falha completamente em separar controles de IPF
+"
 autoplot(prcomp(mpfr.all[,1:9], scale. = T), 
          data = mpfr.all, colour = "treatment", 
-         size = 2)
-autoplot(prcomp(mpfr.all[,1:9], scale. = T), 
-         data = mpfr.all, colour = "times", 
-         size = 2, shape = "treatment")
+         size = 3)
+
 
 # Boxplots
 {
-  day = 3
   ivars <- 1; nvars <- 9
-  filtro = mpfr.all.long$variable %in% unique(mpfr.all.long$variable)[ivars:nvars] &
-    mpfr.all.long$times %in% day
+  filtro = mpfr.all.long$variable %in% unique(mpfr.all.long$variable)[ivars:nvars]
   form <- value ~ treatment
-  
   plot_signif(mpfr.all.long, form,
-              filtro, outTest = T,  method = "t.test",
-              titles = str_glue("Day {day}"), point = T)
+              filtro, outTest = T,  method = "wilcox.test",
+              point = T)
   
 }
 
-# longituginal profile of module8 vs module13
-ggplot(mpfr.all, aes(x = as.numeric(as.character(times)), y = m8m13, 
-                     group = treatment, color = treatment)) +
-  geom_point() + geom_smooth(method = "lm", formula = y ~ poly(x, 3))
-
-
-
 # Modelos
 
-
 {
-  day = 3
-  dat <- fit_mPFR(mpfr.all, day = day, Nvars = 9) 
+  dat <- fit_mPFR(mpfr.all, Nvars = 9) 
   dat$variables <- rownames(dat)
-  }
+}
 
 # volcano
-ggplot(dat, aes(x = Estimate, y = -log10(pval), label = variables)) +
-  geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed", alpha = 0.5) + 
-  theme(legend.title = element_blank()) +
-  geom_text_repel(force = T) +
-  xlab("Bleomycin Effect")
+
+{
+  ggplot(dat, aes(x = Estimate, y = -log10(pval), 
+                  label = variables, size = Rsquared)) +
+    geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) +
+    geom_hline(yintercept = -log10(0.05), linetype = "dashed", alpha = 0.5) + 
+    labs(size = "R squared", color = NULL) +
+    geom_text_repel(force = T) +
+    xlab("IPF effect") + xlim(-6, 6)
+}
 
 # pval vs rsquared
-ggplot(dat, aes(x = -log10(pval), y = Rsquared, 
-                label = variables)) +
-  geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
-  geom_text_repel(nudge_x = 0.02, segment.alpha = 0.3) + 
-  theme(legend.title = element_blank())
+{
+  ggplot(dat, aes(x = -log10(pval), y = Rsquared, 
+                  label = variables)) +
+    geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
+    geom_text_repel(nudge_x = 0.02, segment.alpha = 0.3) + 
+    theme(legend.title = element_blank())
+}
 
 # rsquared vs bleo effect (Estimate)
-ggplot(dat, aes(x = Rsquared, y = Estimate, 
-                label = variables)) +
-  geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
-  geom_text_repel(
-    data = dat[dat$Rsquared >= 0.5, ],
-    segment.alpha =  0.3) + 
-  geom_vline(xintercept = 0.95, alpha = 0.5, linetype = "dashed", color = "blue") +
-  theme(legend.title = element_blank()) +
-  annotate("text", x = 0.88, y = 5.5, color = "blue", alpha = 0.5,
-           label = "R^2 == 0.95", parse = TRUE) +
-  ylab("Bleomycin Effect")
+
+{
+  ggplot(dat, aes(x = Rsquared, y = Estimate, 
+                  label = variables)) +
+    geom_point(aes(color = ifelse(dat$pval < 0.05, "Significant", "Not significant"))) + 
+    geom_text_repel(
+      data = dat[dat$Rsquared >= 0.5, ],
+      segment.alpha =  0.3) + 
+    geom_vline(xintercept = 0.95, alpha = 0.5, linetype = "dashed", color = "blue") +
+    theme(legend.title = element_blank()) +
+    annotate("text", x = 0.85, y = 5.5, color = "blue", alpha = 0.5,
+             label = "R^2 == 0.95", parse = TRUE) +
+    ylab("IPF Effect")
+}
+
+
+
+###### TO-DO LIST ########
+
+"
+  1) Buscar ortologos dos genes dos ratos para humano
+  2) Testar outros moduloes do Xue2014
+  3) Mostrar o N nas facets dos boxplots das variables
+  4) Modelar correlacao entre variables no humanos e nos ratos
+"
+{
+  # Convert rat to human gene names
+  
+  convert_rat_gene_list <- function(x){
+    
+    require("biomaRt")
+    human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+    mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+    
+    mouse_matches = getLDS(attributes = c("mgi_symbol"), 
+                           filters = "mgi_symbol", 
+                           values = x , mart = rat, 
+                           attributesL = c("hgnc_symbol"), 
+                           martL = human, uniqueRows=T)
+    no_matches = setdiff(x, mouse_matches[,1]) 
+    
+    x = data.frame(HGNC.symbol = x)
+    df <- merge(x, mouse_matches, by = "HGNC.symbol")
+    
+    return(list(df, data.frame(no_matches)))
+  }
+  
+  genes <- convert_rat_gene_list(c1not)
+  mymatches <- genes[[1]]
+  nomathes <- genes[[2]]
+  nrow(nomathes) # over 2 thousand genes with no matches
+  nrow(mymatches)
+}
+
+}
